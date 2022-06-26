474da4c4572b8727d22b71acd5a152864d05d920
Elisaveta Zobeva <e.zobeva@gmail.com>
2022-06-26 12:55:20 +0100

    not found page

--- src/pages/notfound/NotFound.js
+++ src/pages/notfound/NotFound.js
@@ -1,1 +1,18 @@
-export default function NotFound() {}
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+
+export default function NotFound() {
+  return (
+    <Box sx={{ display: "flex", justifyContent: "center" }}>
+      <Card sx={{ backgroundColor: "transparent" }}>
+        <CardContent>
+          <Typography sx={{ fontSize: 40 }} gutterBottom>
+            Oh no... Looks like this page doesn't exist :{"("}
+          </Typography>
+        </CardContent>
+      </Card>
+    </Box>
+  );
+}
